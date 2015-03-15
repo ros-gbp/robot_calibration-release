0e1714e7e9341ee4722153b2fedbdf428a996b68
Michael Ferguson <mfergs7@gmail.com>
2015-03-15 02:11:41 -0700

    Rebase from 'upstream'

--- robot_calibration/include/robot_calibration/capture/chain_manager.h
+++ robot_calibration/include/robot_calibration/capture/chain_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Fetch Robotics Inc.
+ * Copyright (C) 2014-2015 Fetch Robotics Inc.
  * Copyright (C) 2013-2014 Unbounded Robotics Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,14 +21,14 @@
 #define ROBOT_CALIBRATION_CAPTURE_CHAIN_MANAGER_H
 
 #include <ros/ros.h>
+#include <boost/thread/mutex.hpp>
 #include <sensor_msgs/JointState.h>
-#include <control_msgs/FollowJointTrajectoryAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <moveit_msgs/MoveGroupAction.h>
 
 namespace robot_calibration
 {
-
-typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> TrajectoryClient;
 
 /**
  * @brief Manages moving joints to a new pose, determining when they
@@ -36,17 +36,31 @@ typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>
  */
 class ChainManager
 {
+  typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> TrajectoryClient;
+  typedef actionlib::SimpleActionClient<moveit_msgs::MoveGroupAction> MoveGroupClient;
+  typedef boost::shared_ptr<MoveGroupClient> MoveGroupClientPtr;
+  typedef boost::shared_ptr<const moveit_msgs::MoveGroupResult> MoveGroupResultPtr;
+
+  // This controls a single chain
   struct ChainController
   {
     ChainController(const std::string& name,
-                    const std::string& topic) :
+                    const std::string& topic,
+                    const std::string& planning_group) :
       client(topic, true),
-      chain_name(name)
+      chain_name(name),
+      chain_planning_group(planning_group)
     {
+    }
+
+    bool shouldPlan()
+    {
+      return (chain_planning_group != "");
     }
 
     TrajectoryClient client;
     std::string chain_name;
+    std::string chain_planning_group;
     std::vector<std::string> joint_names;
   };
 
@@ -59,8 +73,8 @@ public:
   ChainManager(ros::NodeHandle& nh, double wait_time = 15.0);
 
   /**
-   * @brief Send commands to all managed joints. The ChainManager automatically figures out
-   *        which controller to send these to.
+   * @brief Send commands to all managed joints. The ChainManager automatically
+   *        figures out which controller to send these to.
    * @returns False if failed.
    */
   bool moveToState(const sensor_msgs::JointState& state);
@@ -85,6 +99,9 @@ public:
    */
   std::vector<std::string> getChainJointNames(const std::string& chain_name);
 
+  // Mainly for testing
+  std::string getPlanningGroupName(const std::string& chain_name);
+
 private:
   void stateCallback(const sensor_msgs::JointStateConstPtr& msg);
 
@@ -93,7 +110,10 @@ private:
 
   ros::Subscriber subscriber_;
   sensor_msgs::JointState state_;
+  double duration_;
+  boost::mutex state_mutex_;
   std::vector<boost::shared_ptr<ChainController> > controllers_;
+  MoveGroupClientPtr move_group_;
 };
 
 }  // namespace robot_calibration
